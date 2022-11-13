9fe2b5fc79dc9fde92e3dc3c3484fa5946b2a294
Bennett Grow <60197444+bennettgrow@users.noreply.github.com>
2022-11-12 18:51:09 -0700

    Completed KF_accel with dummy variables

--- KalmanFilterC/KF_run.sh
+++ KalmanFilterC/KF_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###    Compile a C program named KF_run    ###
-gcc -g -o KF_run KF_accel.c CControl/Sources/LinearAlgebra/mul.c CControl/Sources/Miscellaneous/print.c 
+gcc -g -o KF_run KF_accel.c CControl/Sources/LinearAlgebra/mul.c CControl/Sources/Miscellaneous/print.c CControl/Sources/LinearAlgebra/tran.c CControl/Sources/LinearAlgebra/inv.c CControl/Sources/LinearAlgebra/lup.c
 
 ###    Run the compiled script    ###
 ./KF_run
