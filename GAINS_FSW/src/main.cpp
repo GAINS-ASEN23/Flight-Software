ffa03085bbee20442b31fb6213f8cbe4258e9df7
Bennett Grow <60197444+bennettgrow@users.noreply.github.com>
2023-02-09 13:17:13 -0700

    KF function timing

--- GAINS_FSW/src/main.cpp
+++ GAINS_FSW/src/main.cpp
@@ -10,7 +10,7 @@
 
 #include <Arduino.h>
 #include <GAINSKF.h>
-
+#include <chrono>
 //#include <time.h> 						// For srand, clock
 
 void setup()
@@ -24,17 +24,26 @@ void loop()
 {
 	delay(2000);
 	Serial.println("Initialized...");
+	delay(1000);
 
 
 	for (int i = 0; i<1000; i++)
 	{
 
 		digitalWrite(LED_BUILTIN, HIGH); 
+		
+		auto begin = std::chrono::high_resolution_clock::now();
+
 		KF();
+
+		auto end = std::chrono::high_resolution_clock::now();
+    	auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    
 		digitalWrite(LED_BUILTIN, LOW);
 
+		Serial.println(elapsed.count());
 
-		//delay(1000);
+		delay(1000);
 
 	}
 
