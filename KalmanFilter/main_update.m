0ee10ee1e6766dea4c154ebfc292f309dc31da97
Bennett Grow <60197444+bennettgrow@users.noreply.github.com>
2023-03-02 18:09:39 -0700

    Merged Jason and KA's code

--- KalmanFilter/main_update.m
+++ KalmanFilter/main_update.m
@@ -49,10 +49,6 @@ mu_moon = 4.9048695e12;                 % [m^3 s^-2] Gravitational parameter of 
 a_moon = 1.74e6+5e4;                    % [m] Semimajor axis of Moon's orbit around Earth
 S.n = sqrt(mu_moon/(a_moon^3));         % [rad/s] Mean motion of the Moon around the Sun (max values for a used)
 
-% P = 29.530 * 24 * 3600; % [sec] Orbital period of the Moon around the Sun
-% S.n = 2*pi/P;           % [rad/s] Mean motion of the Moon around the Sun
-
-
 % Clohessy-Wiltshire equations form A matrix
 S.A = [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1; 3*S.n^2 0 0 0 2*S.n 0; 0 0 0 -2*S.n 0 0; 0 0 -S.n^2 0 0 0];
 
@@ -61,6 +57,8 @@ S.B = [zeros(3); eye(3)];
 
 % State transistion Matrix
 S.F = expm(S.A);
+% Need to add dt above?
+
 
 % Accelerometer uncertanties
 S.sigma1 = 0.1;
--- KalmanFilterCW/KF_cw.m
+++ KalmanFilterCW/KF_cw.m
@@ -1,4 +1,4 @@
-function [state, error] = KF_cw(M_n, U_n, x_n_n, P_n_n, R_n)
+function [state, error] = KF_cw(M_n, U_n, x_n_n, P_n_n, R_n, t)
 %{
     Last Edited: Jason Popich 02/28/23
 
@@ -28,10 +28,12 @@ function [state, error] = KF_cw(M_n, U_n, x_n_n, P_n_n, R_n)
     n = sqrt(mu_moon/(a_moon^3));
     
     % Define A
-    A = [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1; 3*(n^2) 0 0 0 2*n 0; 0 0 0 -2*n 0 0; 0 0 -n^2 0 0 0];
+    %A = [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1; 3*(n^2) 0 0 0 2*n 0; 0 0 0 -2*n 0 0; 0 0 -n^2 0 0 0];
 
     % Define the State Transition Matrix
-    F = expm(A);
+    %F = expm(A*t);
+    F = [4-3*cos(n*t) 0 0 sin(n*t)/n -2*cos(n*t)/n+2/n 0; 6*(sin(n*t)-(n*t)) 1 0 -2*(1-cos(n*t))/n (4*sin(n*t)-3*n*t)/n 0; 0 0 cos(n*t) 0 0 sin(n*t)/n; 3*n*sin(n*t) 0 0 cos(n*t) 2*sin(n*t) 0; -6*n*(1-cos(n*t)) 0 0 -2*sin(n*t) 4*cos(n*t)-3 0; 0 0 -n*sin(n*t) 0 0 cos(n*t)];
+
     
     % Define the input matrix
     B = [zeros(3); eye(3)];
@@ -43,8 +45,8 @@ function [state, error] = KF_cw(M_n, U_n, x_n_n, P_n_n, R_n)
     H = eye(6);
     
     % Set errors
-    pos_sigma = 0.001;              % Position error in [km]
-    vel_sigma = 1e-7;               % Velocity error in [km/s]
+    pos_sigma = 0.1;              % Position error in [km]
+    vel_sigma = 1e-5;               % Velocity error in [km/s]
     
     % Define the Measurement Process Noise Matrix, Q_meas
     Q_meas = [eye(3)*pos_sigma zeros(3); zeros(3) eye(3)*vel_sigma];
