29eeaac2b478bccc1d1cc3b3ab1958a44fb7e93f
Bennett Grow <60197444+bennettgrow@users.noreply.github.com>
2023-03-07 13:09:55 -0700

    Update main.m

--- KalmanFilterCW/main.m
+++ KalmanFilterCW/main.m
@@ -18,63 +18,11 @@ tic;
 %% Sample Data
 sampledata = readmatrix("StarTrackerSample.csv");
 
-n = 100000;                  % Number of data points
+n = 10000;                  % Number of data points
 dt = 0.01;                  % Time step
 t1 = 0;                     % Start time
 t2 = n*dt + t1;             % End time
 t = linspace(t1, t2, n);    % n times from t1 to t1
-
-%% Propagator
-%{
-mu = 4.9048695e12;     %Earth Graitational Constant
-earthRad = 1737.4;       %km
-% Initial Circular Radius of Space Shuttle
-r_int = 500;
-% Create initial coordinates representing the target (Moon)
-xt = earthRad + r_int;
-yt = 0;
-zt = 0;
-xdott = 0;
-ydott = sqrt(mu/(earthRad+r_int));
-zdott = 0;
-rT = [xt, yt, zt]';
-vT = [xdott,ydott,zdott]';
-% Add 51.65 degrees of inclination to the orbit
-inc = -51.65;
-RYM = [cosd(inc) 0 sind(inc); 0 1 0; -sind(inc) 0 cosd(inc)];
-rT = RYM*rT;
-vT = RYM*vT;
-
-% Create initial coordinates representing the chaser satellite (Hubble)
-% Initial Positions and Velocities of the hubble telescope from the space 
-% shuttle bay All units are in km/s
-% Assume that both telescope and shuttle are docked initially
-   xi = rT(1);
-   yi = rT(2);
-   zi = rT(3);
-xdoti = vT(1);
-ydoti = vT(2);
-zdoti = vT(3);
-rI = [xi, yi, zi]';
-vI = [xdoti,ydoti,zdoti]';
-
-[rHill,vHill] = ECI2Hill_Vectorized(rT,vT,rI,vI);
-
-% Add initial displacement on the x vector
-rHill(1) = rHill(1)+1;
-
-% Now ... recompute the chaser satellite vector ...
-[rI,vI] = Hill2ECI_Vectorized(rT,vT,rHill,vHill);
-
-% Find the angular rate of the target orbit (shuttle)
-omega = sqrt(mu/sqrt(sum(rT.^2))^3);
-
-% Use the Linear Propagator (LOP) to propagate Hill's coordinates forward in time
-% This is to find the relative orbital motion of the "Chaser" satellite
-[rHill,vHill] = CWHPropagator(rHill,vHill,omega,t);
-[rHill,vHill] = CWHPropagator(rHillInit,vHillInit,omega,t);
-
-%}
 
 %% Set the initial conditions
 
