fbc889ffc72f2763e6bdbec9636d72d9feb87e87
Bennett Grow <60197444+bennettgrow@users.noreply.github.com>
2023-04-12 11:01:29 -0600

    Revised gitignore to allow other csvs

--- BiasEstimation/temp.m
+++ BiasEstimation/temp.m
@@ -3,7 +3,7 @@ clc; close all; clear all;
 
 
 %% First Test
-
+%{
 d1 = readmatrix(strcat('GAINS 8 Pos Test/GAINS00', '11', '/accel.csv'));
 d2 = readmatrix(strcat('GAINS 8 Pos Test/GAINS00', '12', '/accel.csv'));
 d3 = readmatrix(strcat('GAINS 8 Pos Test/GAINS00', '13', '/accel.csv'));
@@ -31,18 +31,39 @@ ylabel('Bins')
 plot(d(:,1),y,'k',LineWidth=2)
 plot1 = plot(d(:,1),d(:,3),'b');
 plot1.Color(4) = 0.05;
+%}
 
 %% Second Test
-t2 = readmatrix(strcat('GAINS 8 Pos Test/GAINS00', '15', '/accel.csv'));
-t2(:,1) = (t2(:,1)-t2(1,1))./1E6;
-p = polyfit(t2(:,1),t2(:,3),1);
-y2 = polyval(p,t2(:,1));
+ref = readmatrix(strcat('GAINS 8 Pos Test/temp/ref.csv'));
+pref = polyfit(ref(:,1),ref(:,2),3);
+yref = polyval(pref,ref(:,1));
+
+d = readmatrix(strcat('GAINS 8 Pos Test/temp/accel.csv'));
+d(:,1) = (d(:,1)-d(1,1))./1E6;
+p1 = polyfit(d(:,1),d(:,2),1); % Accel
+yd1 = polyval(p1,d(:,1));
+p2 = polyfit(d(:,1),d(:,3),3); % Temp
+yd2 = polyval(p2,d(:,1));
 
 figure;
 hold on;
-title('Second Test')
+title('Oven Test')
 xlabel('Seconds')
+yyaxis left;
 ylabel('Bins')
-plot(t2(:,1),y2,'k',LineWidth=2)
-plot1 = plot(t2(:,1),t2(:,3),'b');
+plot(d(:,1),yd2,'b',LineWidth=2)
+plot1 = plot(d(:,1),d(:,3),'b');
 plot1.Color(4) = 0.05;
+
+yyaxis right;
+ylabel('Degrees C')
+plot(ref(:,1),yref,'r',LineWidth=2)
+scatter(ref(:,1),ref(:,2),'r',Marker='o')
+
+legend('Raw Fit','Raw Data','Ref Temp Fit','Ref Temp Raw',Location='northwest')
+
+figure;
+hold on;
+plot(d(:,1),d(:,2));
+plot(d(:,1),yd1,'r');
+
