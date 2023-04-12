2a7ba0fafc2d5899ab7961356cd9a9bb2cef91e3
japopich <japopich@hotmail.com>
2023-04-11 18:26:58 -0600

    WIP ccsds space packet protocol implementation

---
+++ GAINS_FSW/lib/GAINS/ccsds.cpp
@@ -0,0 +1,186 @@
+/* CCSDS Space Protocol Packet Functions File
+* GAINS Senior Project Capstone Ground Software Backend
+* Authors: Jason Popich, Brian Trybus, Cannon Palmer, Tucker Peyok, Ben McHugh
+* Purpose: This has functions help with using the CCSDS Space Packet Structs
+*/
+
+#include "ccsds.h"
+
+CCSDS_PriHdr_t writeHeader(int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag) 
+{
+    CCSDS_PriHdr_t header;
+    uint16_t StreamID = (0x07FF & apID) | ((0x1 & secondHeader) << 11) | ((0x1 & type) << 12) | ((0x7 & version) << 13);
+    header.StreamId[0] = (0xFF00 & StreamID) >> 8;
+    header.StreamId[1] = (0xFF & StreamID);
+
+    uint16_t Sequence = (0x3FFF & seqCount) | ((0x3 & segFlag) << 14);
+    header.Sequence[0] = (0xFF00 & Sequence) >> 8;
+    header.Sequence[1] = (0xFF & Sequence);
+
+    header.Length[0] = 0;
+    header.Length[1] = 0;
+
+    return header;
+}
+
+headerData readHeader(CCSDS_PriHdr_t hdr) 
+{
+    headerData header;
+
+    header.appId = (int)(((0x07 & hdr.StreamId[0]) << 8) | hdr.StreamId[1]);
+    header.secondHeader = (bool)((0x08 & hdr.StreamId[0])>>3);
+    header.type = (bool)((0x10 & hdr.StreamId[0])>>4);
+    header.version = (int)((0xE0 & hdr.StreamId[0])>>5);
+
+    header.seqCount = (int)(((0x3F & hdr.Sequence[0]) << 8) | hdr.Sequence[1]);
+    header.segFlag = (int)((0xC0 & hdr.Sequence[0])>>6);
+
+    uint16_t Length = (int)((hdr.Length[0] << 8) | hdr.Length[1]);
+    header.length = (int)(Length);
+
+    return header;
+}
+
+GAINS_TLM_PACKET GAINS_TLM_PACKET_constructor(double position_x, double position_y, double position_z, double velocity_x, double velocity_y, double velocity_z, float time, int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag) 
+{
+    GAINS_TLM_PACKET tlmPacket;
+
+    tlmPacket.position_x = position_x;
+    tlmPacket.position_y = position_y;
+    tlmPacket.position_z = position_z;
+    tlmPacket.velocity_x = velocity_x;
+    tlmPacket.velocity_y = velocity_y;
+    tlmPacket.velocity_z = velocity_z;
+
+    tlmPacket.FullHeader.SpacePacket.Hdr = writeHeader(apID, secondHeader, type, version, seqCount, segFlag);
+
+    uint16_t Length = sizeof(tlmPacket);
+    tlmPacket.FullHeader.SpacePacket.Hdr.Length[0] = (0xFF00 & Length) >> 8;
+    tlmPacket.FullHeader.SpacePacket.Hdr.Length[1] = (0xFF & Length);
+
+    tlmPacket.FullHeader.Sec.Time = time;
+    tlmPacket.FullHeader.Sec.Mode = 0; // Telemetry Packets are defined as using mode 0
+
+    return tlmPacket;
+}
+
+GAINS_STAR_PACKET GAINS_STAR_PACKET_constructor(double betaAngle1, double betaAngle2, double betaAngle3, double betaAngle4, float time, int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag) 
+{
+    GAINS_STAR_PACKET starPacket;
+
+    starPacket.betaAngle1 = betaAngle1;
+    starPacket.betaAngle2 = betaAngle2;
+    starPacket.betaAngle3 = betaAngle3;
+    starPacket.betaAngle4 = betaAngle4;
+
+    starPacket.FullHeader.SpacePacket.Hdr = writeHeader(apID, secondHeader, type, version, seqCount, segFlag);
+
+    size_t Length = sizeof(starPacket);
+    starPacket.FullHeader.SpacePacket.Hdr.Length[0] = (0xFF00 & Length) >> 8;
+    starPacket.FullHeader.SpacePacket.Hdr.Length[1] = 0xFF & Length;
+
+    starPacket.FullHeader.Sec.Time = time;
+    starPacket.FullHeader.Sec.Mode = 1; // Star Packets are defined as using mode 1
+
+    return starPacket;
+}
+
+GAINS_TLM_PACKET read_TLM_Packet(uint8_t* recv_buffer) 
+{ 
+    // we need to program this to decode a buffer (an array of uint8_t) and put it into the SPP format
+   // incoming buffer will be 72 bytes long (72 uint8_t). Data from buffer comes in Little Endian
+    GAINS_TLM_PACKET data;
+
+    int offset = 0;
+
+    data.FullHeader.SpacePacket.Hdr.StreamId[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.StreamId[1] = recv_buffer[offset + 1];
+    offset = offset + 2;
+
+    data.FullHeader.SpacePacket.Hdr.Sequence[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.Sequence[1] = recv_buffer[offset + 1];
+    offset = offset + 2;
+
+    data.FullHeader.SpacePacket.Hdr.Length[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.Length[1] = recv_buffer[offset + 1];
+    offset = offset + 2 + 2;
+
+    memcpy(&data.FullHeader.Sec.Time, &recv_buffer[offset], sizeof(data.FullHeader.Sec.Time));
+    offset = offset + 4;
+
+    data.FullHeader.Sec.Mode = recv_buffer[offset];
+    offset = offset + 1 + 3;
+
+    data.ci_command_error_count = recv_buffer[offset];
+    offset = offset + 1 + 7;
+
+    memcpy(&data.position_x, &recv_buffer[offset], sizeof(data.position_x));
+    offset = offset + 8;
+
+    memcpy(&data.position_y, &recv_buffer[offset], sizeof(data.position_y));
+    offset = offset + 8;
+
+    memcpy(&data.position_z, &recv_buffer[offset], sizeof(data.position_z));
+    offset = offset + 8;
+
+    memcpy(&data.velocity_x, &recv_buffer[offset], sizeof(data.velocity_x));
+    offset = offset + 8;
+
+    memcpy(&data.velocity_y, &recv_buffer[offset], sizeof(data.velocity_y));
+    offset = offset + 8;
+
+    memcpy(&data.velocity_z, &recv_buffer[offset], sizeof(data.velocity_z));
+    offset = offset + 8;
+
+    return data;
+}
+
+GAINS_STAR_PACKET read_STAR_Packet(uint8_t* recv_buffer) 
+{ 
+    // we need to program this to decode a buffer (an array of uint8_t) and put it into the SPP format
+    // incoming buffer will be 72 bytes long (72 uint8_t). Data from buffer comes in Little Endian
+    GAINS_STAR_PACKET data;
+
+    int offset = 0;
+
+    data.FullHeader.SpacePacket.Hdr.StreamId[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.StreamId[1] = recv_buffer[offset + 1];
+    offset = offset + 2;
+
+    data.FullHeader.SpacePacket.Hdr.Sequence[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.Sequence[1] = recv_buffer[offset + 1];
+    offset = offset + 2;
+
+    data.FullHeader.SpacePacket.Hdr.Length[0] = recv_buffer[offset];
+    data.FullHeader.SpacePacket.Hdr.Length[1] = recv_buffer[offset + 1];
+    offset = offset + 2 + 2;
+
+    memcpy(&data.FullHeader.Sec.Time, &recv_buffer[offset], sizeof(data.FullHeader.Sec.Time));
+    offset = offset + 4;
+
+    data.FullHeader.Sec.Mode = recv_buffer[offset];
+    offset = offset + 1 + 3;
+
+    data.ci_command_error_count = recv_buffer[offset];
+    offset = offset + 1 + 7;
+
+    memcpy(&data.betaAngle1, &recv_buffer[offset], sizeof(data.betaAngle1));
+    offset = offset + 8;
+
+    memcpy(&data.betaAngle2, &recv_buffer[offset], sizeof(data.betaAngle2));
+    offset = offset + 8;
+
+    memcpy(&data.betaAngle3, &recv_buffer[offset], sizeof(data.betaAngle3));
+    offset = offset + 8;
+
+    memcpy(&data.betaAngle4, &recv_buffer[offset], sizeof(data.betaAngle4));
+    offset = offset + 8;
+
+    memcpy(&data.filler1, &recv_buffer[offset], sizeof(data.filler1));
+    offset = offset + 8;
+
+    memcpy(&data.filler2, &recv_buffer[offset], sizeof(data.filler2));
+    offset = offset + 8;
+
+    return data;
+}
---
+++ GAINS_FSW/lib/GAINS/ccsds.h
@@ -0,0 +1,149 @@
+#pragma once
+/* CCSDS Space Protocol Packet Header File
+* GAINS Senior Project Capstone Ground Software Backend
+* Authors: Jason Popich, Brian Trybus, Cannon Palmer, Tucker Peyok, Ben McHugh
+* Purpose: This is the header file which defines the CCSDS Space Packet Struct Format
+*/
+
+#ifndef _CCSDS_H_
+#define _CCSDS_H_
+
+#include <iostream>       // std::cout
+#include <Arduino.h>
+
+/*
+** Type Definitions
+*/
+
+#define CCSDS_TIME_SIZE 8
+
+/**********************************************************************
+** Structure definitions for CCSDS headers.  All items in the structure
+** must be aligned on 16-bit words.  Bitfields must be avoided since
+** some compilers (such as gcc) force them into 32-bit alignment.
+**
+** CCSDS headers must always be in network byte order per the standard.
+** MSB at the lowest address which is commonly refered to as "BIG Endian"
+**
+** CCSDS Space Packets can be version 1 or version 2.  Version 2 has
+** an additional 32 bits for APID Qualifier fields in the secondary
+** header. The primary header is unchanged.
+**
+**********************************************************************/
+
+/*----- CCSDS packet primary header. -----*/
+
+typedef struct {
+
+    uint8_t   StreamId[2];  /* packet identifier word (stream ID) */
+    /*  bits  shift   ------------ description ---------------- */
+    /* 0x07FF    0  : application ID                            */
+    /* 0x0800   11  : secondary header: 0 = absent, 1 = present */
+    /* 0x1000   12  : packet type:      0 = TLM, 1 = CMD        */
+    /* 0xE000   13  : CCSDS version:    0 = ver 1, 1 = ver 2    */
+
+    uint8_t   Sequence[2];  /* packet sequence word */
+    /*  bits  shift   ------------ description ---------------- */
+    /* 0x3FFF    0  : sequence count                            */
+    /* 0xC000   14  : segmentation flags:  3 = complete packet  */
+
+    uint8_t  Length[2];     /* packet length word */
+    /*  bits  shift   ------------ description ---------------- */
+    /* 0xFFFF    0  : (total packet length) - 7                 */
+
+} CCSDS_PriHdr_t;
+
+/*----- CCSDS command secondary header. -----*/
+
+typedef struct {
+
+    uint16_t  Command;      /* command secondary header */
+    /*  bits  shift   ------------ description ---------------- */
+    /* 0x00FF    0  : checksum, calculated by ground system     */
+    /* 0x7F00    8  : command function code                     */
+    /* 0x8000   15  : reserved, set to 0                        */
+
+} CCSDS_CmdSecHdr_t;
+
+/*----- CCSDS telemetry secondary header. -----*/
+
+typedef struct {
+
+    float  Time;  /* packet time float */
+    uint8_t Mode; /* packet mode word */
+
+} CCSDS_TlmSecHdr_t; 
+
+typedef struct
+{
+    /*
+     * In Version 1 mode, the standard / non-APID qualified header is used for all packets
+     */
+    CCSDS_PriHdr_t      Hdr;    /**< Complete "version 1" (standard) header */
+
+} CCSDS_SpacePacket_t;
+
+/*----- Generic combined command header. -----*/
+
+typedef struct
+{
+    CCSDS_SpacePacket_t  SpacePacket;   /**< \brief Standard Header on all packets  */
+    CCSDS_CmdSecHdr_t    Sec;
+} CCSDS_CommandPacket_t;
+
+/*----- Generic combined telemetry header. -----*/
+
+typedef struct
+{
+    CCSDS_SpacePacket_t  SpacePacket; /**< \brief Standard Header on all packets */
+    CCSDS_TlmSecHdr_t    Sec;
+} CCSDS_TelemetryPacket_t;
+
+/*----- Combined telemetry packet -----*/
+struct GAINS_TLM_PACKET { /* Length will be 72 bytes due to data alignment */
+    CCSDS_TelemetryPacket_t  FullHeader;
+    uint8_t     ci_command_error_count{ 0 };
+    double      position_x{ 0 };
+    double      position_y{ 0 };
+    double      position_z{ 0 };
+    double      velocity_x{ 0 };
+    double      velocity_y{ 0 };
+    double      velocity_z{ 0 };
+};
+
+/*----- Combined Star Tracker packet -----*/
+struct GAINS_STAR_PACKET { /* Length will be 72 bytes due to data alignment */
+    CCSDS_TelemetryPacket_t     FullHeader;
+    uint8_t     ci_command_error_count{ 0 };
+    double      betaAngle1{ 0 };
+    double      betaAngle2{ 0 };
+    double      betaAngle3{ 0 };
+    double      betaAngle4{ 0 };
+    double      filler1 = 0;
+    double      filler2 = 0;
+
+};
+
+struct headerData {
+    int appId = 0;
+    bool secondHeader = 0;
+    bool type = 0;
+    int version = 0;
+    int seqCount = 0;
+    int segFlag = 0;
+    int length = 0;
+};
+
+
+#define CFE_SB_TLM_HDR_SIZE sizeof(CCSDS_TelemetryPacket_t)
+
+CCSDS_PriHdr_t writeHeader(int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag);
+headerData readHeader(CCSDS_PriHdr_t hdr);
+
+GAINS_TLM_PACKET GAINS_TLM_PACKET_constructor(double position_x, double position_y, double position_z, double velocity_x, double velocity_y, double velocity_z, float time, int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag);
+GAINS_STAR_PACKET GAINS_STAR_PACKET_constructor(double betaAngle1, double betaAngle2, double betaAngle3, double betaAngle4, float time, int apID, bool secondHeader, bool type, int version, int seqCount, int segFlag);
+
+GAINS_TLM_PACKET read_TLM_Packet(uint8_t* recv_buffer);
+GAINS_STAR_PACKET read_STAR_Packet(uint8_t* recv_buffer);
+
+#endif
